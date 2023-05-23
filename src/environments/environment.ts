4896c8bc00ea988115fa9fa92f8ba8995da4e985
Praveen0421 <raipraveen421@gmail.com>
2023-05-23 19:18:59 +0530

    change validation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,30 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+
+  production: false,
+// 1Authority Configuration
+client_id: 'DeskBook',
+// openid profile - UserProfile
+scope: 'openid profile UserProfile 1AuthorityApi 1RPPPolicyServerApi DeskBookApi',
+response_type: 'id_token token',
+authority: 'https://1authority-interns.1rivet.com:5004/',
+redirect_uri: 'http://localhost:4200/',
+secret: 'secret1',
+grant_type: 'implicit',
+// Policy server config
+policy_url: 'http://103.249.120.58:8503/api/userPolicy/',
+policy_name: 'PMCPolicy',
+apiUrl: 'http://103.249.120.58:8508/api/',
+authority_register_uri: 'https://deskbook-admin-service-dev.1rivet.com:9445/api/',
+acr_values: 'tenant:92E838C3-EC40-486F-B126-BBEC78E7D99B',
+userprofile_uri :"https://dev-deskbook-userapi.1rivet.com:9443/api/deskbook/"
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+//  scopes are used by an application during authentication to authorize access to a user's details, like name and picture.
+
+// 
